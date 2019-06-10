bf70caba1cc6d40ba4b4dcb7c871c1bb7a8b499f
Piotr Falkowski <piotr.falkowski@opengi.pl>
2019-06-10 17:47:06 +0200

    async

--- GetWorkingHours/ViewModel.cs
+++ GetWorkingHours/ViewModel.cs
@@ -5,11 +5,17 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Commands;
 
 namespace GetWorkingHours
 {
     public class ViewModel : BindableBase
     {
+        public ViewModel()
+        {
+            CalculateResultCommand = new DelegateCommand(async () => await CalculateResultAsync());
+        }
         private DateTime _DateFrom = DateTime.Now;
         public DateTime DateFrom
         {
@@ -22,9 +28,7 @@ namespace GetWorkingHours
                 if (value != _DateFrom)
                 {
                     _DateFrom = value;
-                    TextBoxContent = GetDiffBetweenChoosenDates().ToString();
                     RaisePropertyChanged();
-                    RaisePropertyChanged(nameof(TextBoxContent));
                 }
             }
         }
@@ -40,13 +44,36 @@ namespace GetWorkingHours
                 if (value != _DateTo)
                 {
                     _DateTo = value;
-                    TextBoxContent = GetDiffBetweenChoosenDates().ToString();
                     RaisePropertyChanged();
-                    RaisePropertyChanged(nameof(TextBoxContent));
                 }
             }
         }
-        public string TextBoxContent { get; set; }
+        private string _textBoxContent;
+        public string TextBoxContent
+        {
+            get
+            {
+                return _textBoxContent;
+            }
+            set
+            {
+                if (value != _textBoxContent)
+                {
+                    _textBoxContent = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public ICommand CalculateResultCommand {get; set; }
+        private async Task CalculateResultAsync()
+        {
+            TextBoxContent = FormatTimespanUserFriendly(await GetDiffBetweenChoosenDates());
+        }
+        private string FormatTimespanUserFriendly(TimeSpan span)
+        {
+            return $"{Math.Round(span.TotalHours, 2)} hour(s)";
+        }
         private static string WindowsIdentityName { get; } = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
         private static string FormattedIdentityName
         {
@@ -63,7 +90,7 @@ namespace GetWorkingHours
             return logon;
         }
         private static Lazy<IEnumerable<EventLogEntry>> LazyLog { get; set; } = new Lazy<IEnumerable<EventLogEntry>>(() => { return EagerLog(); });
-        private static Lazy<IEnumerable<EventLogEntry>> LazyLogOnLog { get; set; } = new Lazy<IEnumerable<EventLogEntry>>(() => 
+        private static Lazy<IEnumerable<EventLogEntry>> LazyLogOnLog { get; set; } = new Lazy<IEnumerable<EventLogEntry>>(() =>
         {
             return LazyLog.Value.Where(entry => entry.InstanceId == 4624 && entry.Message.Contains(FormattedIdentityName));
         });
@@ -72,53 +99,58 @@ namespace GetWorkingHours
             return LazyLog.Value.Where(entry => entry.InstanceId == 4634 && entry.Message.Contains(FormattedIdentityName));
         });
 
-        private TimeSpan GetDiffBetweenChoosenDates()
+        private async Task<TimeSpan> GetDiffBetweenChoosenDates()
         {
             if (DateFrom.Date > DateTo.Date) throw new ArgumentException(nameof(DateTo));
             var currentCheck = DateFrom;
             TimeSpan summaryDiff = new TimeSpan();
             while (currentCheck <= DateTo)
             {
-                var firstLogon = GetFirstSecurityLogonThatDay(currentCheck);
-                var lastLogon = GetLastSecurityLogoffThatDay(currentCheck);
-                if (firstLogon.Date != currentCheck) throw new IndexOutOfRangeException(nameof(firstLogon));
-                if (lastLogon.Date != currentCheck) throw new IndexOutOfRangeException(nameof(lastLogon));
+                var firstLogon = await GetFirstSecurityLogonThatDay(currentCheck).ConfigureAwait(false);
+                var lastLogon = await GetLastSecurityLogoffThatDay(currentCheck).ConfigureAwait(false);
+                if (firstLogon.Date != currentCheck.Date) throw new IndexOutOfRangeException(nameof(firstLogon));
+                if (lastLogon.Date != currentCheck.Date) throw new IndexOutOfRangeException(nameof(lastLogon));
                 if (lastLogon < firstLogon) throw new IndexOutOfRangeException(nameof(lastLogon));
                 summaryDiff += lastLogon - firstLogon;
                 currentCheck = currentCheck.AddDays(1);
             }
             return summaryDiff;
         }
-        public DateTime GetFirstSecurityLogonThatDay(DateTime date)
+        public async Task<DateTime> GetFirstSecurityLogonThatDay(DateTime date)
         {
             DateTime result = DateTime.MaxValue;
-            foreach (var entry in LazyLogOnLog.Value)
+            await Task.Run(() =>
             {
-                if (entry.TimeWritten.Date == date.Date)
+                foreach (var entry in LazyLogOnLog.Value)
                 {
-                    if (result > entry.TimeWritten)
+                    if (entry.TimeWritten.Date == date.Date)
                     {
-                        result = entry.TimeWritten;
+                        if (result > entry.TimeWritten)
+                        {
+                            result = entry.TimeWritten;
+                        }
                     }
                 }
-            }
+            }).ConfigureAwait(false);
             if (result == DateTime.MaxValue)
                 return date;
             else return result;
         }
-        public DateTime GetLastSecurityLogoffThatDay(DateTime date)
+        public async Task<DateTime> GetLastSecurityLogoffThatDay(DateTime date)
         {
-            DateTime result = DateTime.MinValue;
-            foreach (var entry in LazyLogOffLog.Value)
+            DateTime result = DateTime.MinValue; await Task.Run(() =>
             {
-                if (entry.TimeWritten.Date == date.Date)
+                foreach (var entry in LazyLogOffLog.Value)
                 {
-                    if (result < entry.TimeWritten)
+                    if (entry.TimeWritten.Date == date.Date)
                     {
-                        result = entry.TimeWritten;
+                        if (result < entry.TimeWritten)
+                        {
+                            result = entry.TimeWritten;
+                        }
                     }
                 }
-            }
+            }).ConfigureAwait(false);
             if (result == DateTime.MinValue)
                 return date;
             else return result;
